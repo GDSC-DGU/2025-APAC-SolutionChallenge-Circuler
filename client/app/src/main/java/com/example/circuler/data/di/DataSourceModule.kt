b8f97a1542831c3db69f0f46f84c86805decb040
kimsuhyeon <suhyun10201@naver.com>
2025-04-20 16:21:48 +0900

    setting/#1: 서버 통신 module 추가

---
+++ client/app/src/main/java/com/example/circuler/data/di/DataSourceModule.kt
@@ -0,0 +1,12 @@
+package com.example.circuler.data.di
+
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+
+@Module
+@InstallIn(SingletonComponent::class)
+internal abstract class DataSourceModule {
+
+}
