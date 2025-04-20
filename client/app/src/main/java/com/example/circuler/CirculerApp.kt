983385d82f2e5ee57c85bf3c4e4e1131a87c72d1
kimsuhyeon <suhyun10201@naver.com>
2025-04-20 16:03:30 +0900

    setting/#1: 다크 모드 차단

--- client/app/src/main/java/com/example/circuler/CirculerApp.kt
+++ client/app/src/main/java/com/example/circuler/CirculerApp.kt
@@ -1,9 +1,15 @@
 package com.example.circuler
 
 import android.app.Application
+import androidx.appcompat.app.AppCompatDelegate
 
 class CirculerApp : Application() {
     override fun onCreate() {
         super.onCreate()
+        setDarkMode()
+    }
+
+    private fun setDarkMode(){
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
     }
 }
