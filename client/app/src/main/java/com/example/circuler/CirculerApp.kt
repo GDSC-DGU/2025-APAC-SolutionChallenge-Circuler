bf005f1ab1b81b76f2c347be6152979f52e18f3a
kimsuhyeon <suhyun10201@naver.com>
2025-04-20 16:04:36 +0900

    setting/#1: Timber 설정

--- client/app/src/main/java/com/example/circuler/CirculerApp.kt
+++ client/app/src/main/java/com/example/circuler/CirculerApp.kt
@@ -2,14 +2,20 @@ package com.example.circuler
 
 import android.app.Application
 import androidx.appcompat.app.AppCompatDelegate
+import timber.log.Timber
 
 class CirculerApp : Application() {
     override fun onCreate() {
         super.onCreate()
+        setTimber()
         setDarkMode()
     }
 
-    private fun setDarkMode(){
+    private fun setTimber() {
+        if (BuildConfig.DEBUG) Timber.plant(Timber.DebugTree())
+    }
+
+    private fun setDarkMode() {
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
     }
 }
