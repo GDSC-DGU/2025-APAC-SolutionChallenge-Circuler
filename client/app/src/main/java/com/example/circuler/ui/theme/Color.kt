14b97bb9777f2aa2ccfe07f00d55b1e08217de15
kimsuhyeon <suhyun10201@naver.com>
2025-04-20 17:40:02 +0900

    setting/#2: 색상 초기 세팅

--- client/app/src/main/java/com/example/circuler/ui/theme/Color.kt
+++ client/app/src/main/java/com/example/circuler/ui/theme/Color.kt
@@ -1,11 +1,21 @@
 package com.example.circuler.ui.theme
 
+import androidx.compose.runtime.Immutable
+import androidx.compose.runtime.staticCompositionLocalOf
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val White = Color(0xFFFFFFFF)
+val Black = Color(0xFF000000)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+@Immutable
+data class CirculerColors(
+    val White: Color,
+    val Black: Color,
+)
+
+val defaultCirculerColors = CirculerColors(
+    White = White,
+    Black = Black
+)
+
+val LocalColors = staticCompositionLocalOf { defaultCirculerColors }
