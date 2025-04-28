d89682814b6fb15b90ee3e0d519828a5c9ef7906
Lee Jihee <129031670+munjji@users.noreply.github.com>
2025-04-28 14:45:20 +0900

    setting: Entity 설정

---
+++ server/src/main/java/circulo/server/domain/packagingRequest/entity/enums/PackagingType.java
@@ -0,0 +1,11 @@
+package circulo.server.domain.packagingRequest.entity.enums;
+
+public enum PackagingType {
+    ICE_PACK,
+    COOLER_BAG,
+    BUBBLE_WRAP,
+    AIR_POCKET,
+    PAPER_CUSHION,
+    STYROFOAM_BOX,
+    SHOPPING_BAG
+}
