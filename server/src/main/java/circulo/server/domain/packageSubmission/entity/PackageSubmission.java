3be8b197b6ae201f62a2c9f9fb3ba9bdebece524
Lee Jihee <129031670+munjji@users.noreply.github.com>
2025-04-30 11:22:01 +0900

    refactor: PackageSubmission User 연관관계 누락 수정

--- server/src/main/java/circulo/server/domain/packageSubmission/entity/PackageSubmission.java
+++ server/src/main/java/circulo/server/domain/packageSubmission/entity/PackageSubmission.java
@@ -1,8 +1,8 @@
 package circulo.server.domain.packageSubmission.entity;
 
 import circulo.server.domain.packageSubmission.entity.enums.DeliveryMethod;
-import circulo.server.domain.user.entity.User;
 import circulo.server.domain.packagingRequest.entity.PackagingRequest;
+import circulo.server.domain.user.entity.User;
 import circulo.server.global.common.domain.BaseEntity;
 import jakarta.persistence.*;
 import lombok.*;
@@ -19,9 +19,15 @@ public class PackageSubmission extends BaseEntity {
     @Column(name = "submission_id")
     private Long id;
 
+    // 포장재 제출을 희밍하는 일반 사용자
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id")
     private User user;
+
+    // 포장재를 요청한 소상공인
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "storeman_id")
+    private User storeMane;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "request_id")
