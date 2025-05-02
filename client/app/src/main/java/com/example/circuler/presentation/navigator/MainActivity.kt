2e48442be9d252ae2c7e988e59a24c0d12594754
kimsuhyeon <suhyun10201@naver.com>
2025-05-02 20:39:14 +0900

    feat/#14: ktlintFormat

--- client/app/src/main/java/com/example/circuler/presentation/navigator/MainActivity.kt
+++ client/app/src/main/java/com/example/circuler/presentation/navigator/MainActivity.kt
@@ -4,8 +4,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.PaddingValues
-import com.example.circuler.presentation.dummy.ReqresRoute
 import com.example.circuler.ui.theme.CirculerTheme
 import dagger.hilt.android.AndroidEntryPoint
 
