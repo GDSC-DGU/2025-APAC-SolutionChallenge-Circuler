726e30e4d18cea3cebd7f449fb362bbdb41a175f
kimsuhyeon <suhyun10201@naver.com>
2025-05-05 22:40:33 +0900

    feature/#38: CirculoLoadingView 구현

--- client/app/src/main/java/com/example/circuler/presentation/ui/upload/UploadPackagingRoute.kt
+++ client/app/src/main/java/com/example/circuler/presentation/ui/upload/UploadPackagingRoute.kt
@@ -2,10 +2,12 @@ package com.example.circuler.presentation.ui.upload
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBarsPadding
@@ -23,6 +25,7 @@ import androidx.lifecycle.compose.LocalLifecycleOwner
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.flowWithLifecycle
 import com.example.circuler.presentation.core.component.CirculoButton
+import com.example.circuler.presentation.core.component.CirculoLoadingView
 import com.example.circuler.presentation.core.extension.showToast
 import com.example.circuler.presentation.core.util.UiState
 import com.example.circuler.ui.theme.CirculerTheme
@@ -71,23 +74,30 @@ fun UploadPackagingScreen(
             .statusBarsPadding()
             .navigationBarsPadding()
     ) {
-        Column(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(start = 20.dp, end = 20.dp, bottom = 12.dp),
-            verticalArrangement = Arrangement.spacedBy(16.dp, alignment = Alignment.CenterVertically),
-        ) {
-            Spacer(
-                modifier = Modifier
-                    .weight(1f)
-            )
+//        Column(
+//            modifier = Modifier
+//                .fillMaxSize()
+//                .padding(start = 20.dp, end = 20.dp, bottom = 12.dp),
+//            verticalArrangement = Arrangement.spacedBy(16.dp, alignment = Alignment.CenterVertically),
+//        ) {
+//            Spacer(
+//                modifier = Modifier
+//                    .weight(1f)
+//            )
+//
+//            CirculoButton(
+//                text = "accept",
+//                onClick = {
+//                    //todo: navigateToSubmit
+//                }
+//            )
+//        }
 
-            CirculoButton(
-                text = "accept",
-                onClick = {
-                    //todo: navigateToSubmit
-                }
-            )
+        Box(
+            modifier = Modifier
+                .height((LocalConfiguration.current.screenHeightDp).dp),
+        ) {
+            CirculoLoadingView()
         }
     }
 }
