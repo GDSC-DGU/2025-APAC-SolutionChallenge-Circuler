9cbf0995cb88a2a97ba82e0f5d221392f60101dd
kimsuhyeon <suhyun10201@naver.com>
2025-05-05 18:50:10 +0900

    refactor/#33: CirculoText 분리

--- client/app/src/main/java/com/example/circuler/presentation/core/component/CirculoListCard.kt
+++ client/app/src/main/java/com/example/circuler/presentation/core/component/CirculoListCard.kt
@@ -9,17 +9,13 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Menu
-import androidx.compose.material3.Icon
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.circuler.domain.entity.ListCardEntity
@@ -154,44 +150,6 @@ fun CirculoListCardWithMethod(
     }
 }
 
-@Composable
-private fun CirculoTextWithIcon(
-    icon: ImageVector,
-    title: String,
-    subTitle: String,
-    modifier: Modifier = Modifier
-) {
-    Row(
-        horizontalArrangement = Arrangement.Center,
-        verticalAlignment = Alignment.CenterVertically
-    ) {
-        Icon(
-            modifier = modifier
-                .size(24.dp),
-            imageVector = icon,
-            contentDescription = null,
-            tint = CirculerTheme.colors.grayScale8
-        )
-        Spacer(
-            modifier = Modifier
-                .size(2.dp)
-        )
-        Text(
-            text = title,
-            style = CirculerTheme.typography.body4R12,
-            color = CirculerTheme.colors.grayScale12
-        )
-        Spacer(
-            modifier = Modifier
-                .size(20.dp)
-        )
-        Text(
-            text = subTitle,
-            style = CirculerTheme.typography.body4R12,
-            color = CirculerTheme.colors.grayScale12
-        )
-    }
-}
 
 @Preview
 @Composable
