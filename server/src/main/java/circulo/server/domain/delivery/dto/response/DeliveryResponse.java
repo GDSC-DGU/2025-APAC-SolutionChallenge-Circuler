c87e341e5161b385b30e85c8fa3196d2b8da919f
김규리 <kimkyuree1@naver.com>
2025-05-07 03:59:48 +0900

    feature/#44: Added DeliveryResponseDto

---
+++ server/src/main/java/circulo/server/domain/delivery/dto/response/DeliveryResponse.java
@@ -0,0 +1,20 @@
+package circulo.server.domain.delivery.dto.response;
+
+import lombok.*;
+
+public class DeliveryResponse {
+
+    @Builder
+    @Getter
+    @AllArgsConstructor(access = AccessLevel.PROTECTED)
+    @NoArgsConstructor(access = AccessLevel.PROTECTED)
+    public static class PackagingRequestDetailResponse {
+        private Long deliveryId;
+        private Integer submissionQuantity;
+        private String submissionLocation;
+        private String packagingType;
+        private String requestLocation;
+        private String userName;
+        private String storemanName;
+    }
+}
