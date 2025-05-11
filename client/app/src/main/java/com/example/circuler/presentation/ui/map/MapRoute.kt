e63c5299ee41af78ca1ec025da6cd79d3727a646
kimsuhyeon <suhyun10201@naver.com>
2025-05-12 00:40:37 +0900

    feature/#60: MapRoute 구현

--- client/app/src/main/java/com/example/circuler/presentation/ui/map/MapRoute.kt
+++ client/app/src/main/java/com/example/circuler/presentation/ui/map/MapRoute.kt
@@ -1,17 +1,22 @@
 package com.example.circuler.presentation.ui.map
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.LocalLifecycleOwner
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.flowWithLifecycle
+import com.example.circuler.presentation.core.component.CirculoButton
 import com.example.circuler.presentation.core.extension.showToast
 import com.example.circuler.presentation.core.util.UiState
 import com.example.circuler.ui.theme.CirculerTheme
@@ -59,22 +64,45 @@ fun MapScreen(
     state: UiState<String>,
     modifier: Modifier = Modifier
 ) {
-    val singapore = LatLng(1.35, 103.87)
+    val dgu = LatLng(37.558391, 127.000227)
+    val shop = LatLng(37.557131, 127.004303)
+
     val cameraPositionState = rememberCameraPositionState {
-        position = CameraPosition.fromLatLngZoom(singapore, 10f)
+        position = CameraPosition.fromLatLngZoom(dgu, 15f)
     }
 
-    GoogleMap(
+    Box(
         modifier = Modifier.fillMaxSize(),
-        cameraPositionState = cameraPositionState
     ) {
-        Marker(
-            state = MarkerState(position = singapore),
-            title = "Singapore",
-            snippet = "Marker in Singapore",
-        )
-    }
+        GoogleMap(
+            cameraPositionState = cameraPositionState
+        ) {
+            Marker(
+                state = MarkerState(position = dgu),
+                title = "동국대학교 서울캠퍼스",
+                snippet = "Marker in DGU",
+            )
 
+            Marker(
+                state = MarkerState(position = shop),
+                title = "하우스 커피 앤 디저트",
+                snippet = "Marker in Cafe",
+            )
+        }
+
+        Box(
+            modifier = Modifier
+                .fillMaxSize(),
+            contentAlignment = Alignment.BottomCenter
+        ) {
+            CirculoButton(
+                modifier = Modifier.padding(start = 20.dp, end = 20.dp, bottom = 40.dp),
+                text = "delivery request",
+                textStyle = CirculerTheme.typography.heading4B18,
+                onClick = navigateToHome
+            )
+        }
+    }
 }
 
 @Preview
